51256c2c2c2981211e94f0239134fbadcbffa3ec
luoshanglin <1039466810@qq.com>
2022-03-23 16:23:46 +0800

    expose AutoWireCapableBeanFactory by BeanFactoryAware interface

--- spring-demo/src/main/java/com/myflx/service/OrderService.java
+++ spring-demo/src/main/java/com/myflx/service/OrderService.java
@@ -1,15 +1,19 @@
 package com.myflx.service;
 
 import com.myflx.dao.OrderDao;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @Service
 //@Component
-public class OrderService implements InitializingBean {
+public class OrderService implements InitializingBean, BeanFactoryAware {
     public OrderService() {
         System.out.println("OrderService construct....");
     }
@@ -31,5 +35,10 @@ public class OrderService implements InitializingBean {
     @PostConstruct
     public void init() throws Exception {
         System.out.println("OrderService initializing000...");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("是不是AutowireCapableBeanFactory:" + (beanFactory instanceof AutowireCapableBeanFactory));
     }
 }
