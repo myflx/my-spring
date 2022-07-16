e038d84303c2e0f57152093aa8ae1547cd68c467
luoshanglin <1039466810@qq.com>
2022-07-16 20:11:41 +0800

    test SpringProxy

--- spring-demo/src/main/java/com/myflx/aspect/LogAspect.java
+++ spring-demo/src/main/java/com/myflx/aspect/LogAspect.java
@@ -1,17 +1,15 @@
 package com.myflx.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Aspect
 @EnableAspectJAutoProxy
 public class LogAspect {
 
     @Before("execution(* com.myflx.dao.OrderDao..*(..))")
-    public void log(){
+    public void log() {
         System.out.println("before aspect.....");
     }
 }
--- spring-demo/src/main/java/com/myflx/processor/MyBeanPostProcessor.java
+++ spring-demo/src/main/java/com/myflx/processor/MyBeanPostProcessor.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
     @Override
     public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
